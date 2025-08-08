8f5ca772d9544e41a0869c8932a9661d2fd26715
LEEJINHEE <dlwlsgml1106@gmail.com>
2025-08-09 01:25:46 +0900

    0809/01:25

---
+++ js/swiper.js
@@ -0,0 +1,17 @@
+const cardSwiper = new Swiper('.card-swiper', {
+  loop: true,
+  spaceBetween: 20,
+  speed: 600,
+
+  navigation: {
+    nextEl: '.card-button-next',
+    prevEl: '.card-button-prev',
+  },
+
+  breakpoints: {
+    0: { slidesPerView: 1, slidesPerGroup: 1, loopAdditionalSlides: 2 },
+    768: { slidesPerView: 2, slidesPerGroup: 2, loopAdditionalSlides: 4 },
+    1025: { slidesPerView: 3, slidesPerGroup: 3, loopAdditionalSlides: 6 },
+    1400: { slidesPerView: 4, slidesPerGroup: 4, loopAdditionalSlides: 8 }
+  }
+});
