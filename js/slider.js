0316434c750633fbbed9b5caa266e72625d96967
LEEJINHEE <dlwlsgml1106@gmail.com>
2025-08-08 17:01:06 +0900

    카드뉴스17:00

--- js/slider.js
+++ js/slider.js
@@ -18,21 +18,40 @@ const swiper = new Swiper('.banner-swiper', {
   },
 });
 
-//카드뉴스 슬라이더
+//카드뉴스
 const cardSwiper = new Swiper('.card-swiper', {
   loop: true,
-  centeredSlides: true,
-  slidesPerView: 3,
+  slidesPerView: 4,
+  slidesPerGroup: 4,
   spaceBetween: 20,
   speed: 600,
+  loopAdditionalSlides: 8, // 데스크탑에서 여유롭게
 
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
+  breakpoints: {
+    0: { 
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      loopAdditionalSlides: 2
+    },
+    768: { 
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      loopAdditionalSlides: 4
+    },
+    1025: { 
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      loopAdditionalSlides: 6
+    },
+    1400: { 
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      loopAdditionalSlides: 8
+    }
+  }
 });
